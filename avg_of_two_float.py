76b98625e4129088038a6189ba768b85305e1c15
Sandeep Singh Rawat <75193459+SandeepRawat5128@users.noreply.github.com>
2024-05-13 11:56:05 +0530

    Create avg_of_two_float.py

---
+++ avg_of_two_float.py
@@ -0,0 +1,6 @@
+#WAP to input two floating point numbers & print their average
+
+a=float(input("Enter first Number: "))
+b=float(input("Enter first Number: "))
+avg=(a+b)/2
+print("Average of Number: ",avg)
